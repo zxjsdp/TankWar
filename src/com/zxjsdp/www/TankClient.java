05ac288e139e118872a8d6fa54a9b6c14f1002e7
zxjsdp <zxjsdp@gmail.com>
2016-02-28 00:30:42 +0800

    Modify TankClient for new Tank class

--- src/com/zxjsdp/www/TankClient.java
+++ src/com/zxjsdp/www/TankClient.java
@@ -15,10 +15,8 @@ public class TankClient extends Frame {
     public static final int WINDOW_INIT_POSITION = 100;
     public static final int GAME_WIDTH = 1400;
     public static final int GAME_HEIGHT = 900;
-    public static final int TANK_SIZE = 30;
 
-    int x = 50;
-    int y = 50;
+    Tank myTank = new Tank(30, 30);
 
     Image offScreenImage = null;
 
@@ -43,12 +41,7 @@ public class TankClient extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        Color c = g.getColor();
-        g.setColor(Color.red);
-        g.fillOval(x, y, TANK_SIZE, TANK_SIZE);
-        g.setColor(c);
-
-//        y += 2;
+        myTank.draw(g);
     }
 
     @Override
@@ -70,7 +63,7 @@ public class TankClient extends Frame {
             while (true) {
                 repaint();
                 try {
-                    Thread.sleep(20);
+                    Thread.sleep(50);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -81,24 +74,7 @@ public class TankClient extends Frame {
     private class KeyMonitor extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-            int key = e.getKeyCode();
-            if (key == KeyEvent.VK_RIGHT) {
-                x += 5;
-            }
-            switch (key) {
-                case KeyEvent.VK_LEFT:
-                    x -= 5;
-                    break;
-                case KeyEvent.VK_UP:
-                    y -= 5;
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    x += 5;
-                    break;
-                case KeyEvent.VK_DOWN:
-                    y += 5;
-                    break;
-            }
+            myTank.keyPressed(e);
         }
     }
 }
