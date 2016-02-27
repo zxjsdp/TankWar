eab241d0173eae53ed97f5f9f76c0dc07f33d90f
zxjsdp <zxjsdp@gmail.com>
2016-02-27 14:36:40 +0000

    Add window closing event and not resizable

--- src/com/zxjsdp/www/TankClient.java
+++ src/com/zxjsdp/www/TankClient.java
@@ -1,21 +1,27 @@
 package com.zxjsdp.www;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 
 /**
  * Created by Jin on 2016/2/27.
  */
+
 public class TankClient extends Frame {
 
     public void launchFrame() {
         this.setLocation(400, 300);
         this.setSize(1200, 800);
+        this.setTitle("TankWar");
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setResizable(false);
         setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        TankClient tc = new TankClient();
-        tc.launchFrame();
     }
 }
